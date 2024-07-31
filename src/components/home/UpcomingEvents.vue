53c939a0d3d822c92108f50d84ee7dd5fa4ac0c1
priyaljain919 <priyaljain919@gmail.com>
2024-07-31 12:52:19 -0700

    updated carousel

--- src/components/home/UpcomingEvents.vue
+++ src/components/home/UpcomingEvents.vue
@@ -1,156 +1,154 @@
-<template>
-    <v-container id="upcoming-events" fluid class="google-font text-white">
-        <div class="">
-            <h2 style="
-            font-size: 36px;
-            font-weight: 600;
-            line-height: 40px;
-            text-align: center;
-          ">
-                Find Latest upcoming events
-            </h2>
-        </div>
-        <v-carousel :show-arrows="false" v-if="chapters.length > 0">
-            <v-carousel-item>
-                <v-row align="center" justify="center" class="py-10">
-                    <template v-for="(eventData) in chapters">
-                        <v-col cols="12" sm="12" md="6" lg="4" v-for="(communityData, evIndex) in eventData"
-                            :key="evIndex">
-                            <v-card class="pa-10"
-                                style="background: #000; border-color:#1f2023; border-width: 2px; border-radius: 60px; border-style: solid;">
-                                <div :class="`pa-5 mb-5 d-flex flex-column align-center ${getStyle(evIndex)}`"
-                                    style="border-radius: 60px;">
-                                    <h1><strong>{{ communityData.city }}</strong></h1>
-                                </div>
 
-                                <h2 class="text-white">On {{ getformatedDate(communityData.date) }}</h2>
-                                <h5 class="text-white">at {{ communityData.place }}</h5>
-                                <div class="d-flex flex-column">
-                                    <v-btn large rounded dark :href="`${communityData.rsvp}`" target="_blank" style="
+  <template>
+    <v-container id="upcoming-events" fluid class="google-font text-white">
+      <div class="">
+        <h2 style="
+          font-size: 36px;
+          font-weight: 600;
+          line-height: 40px;
+          text-align: center;
+        ">
+          Find Our Latest Events
+        </h2>
+      </div>
+  
+      <div v-if="chunkedUpcomingEvents.length > 0">
+        <h3 style="text-align: center; color: #076EFF;">Upcoming Events</h3>
+        <v-carousel :show-arrows="true">
+          <v-carousel-item v-for="(chunk, index) in chunkedUpcomingEvents" :key="index">
+            <v-row align="center" justify="center" class="py-10">
+              <v-col cols="12" sm="12" md="4" lg="4" v-for="(event, evIndex) in chunk" :key="evIndex">
+                <v-card class="pa-10"
+                  style="background: #000; border-color:#1f2023; border-width: 2px; border-radius: 60px; border-style: solid;">
+                  <div :class="`pa-5 mb-5 d-flex flex-column align-center ${getStyle(evIndex)}`"
+                    style="border-radius: 60px;">
+                    <h1><strong>{{ event.city }}</strong></h1>
+                  </div>
+                  <h2 class="text-white">On {{ getFormattedDate(event.date) }}</h2>
+                  <h5 class="text-white">at {{ event.place }}</h5>
+                  <div class="d-flex flex-column">
+                    <v-btn large rounded dark :href="`${event.rsvp}`" target="_blank" style="
                       background: linear-gradient(145.24deg,#076eff 541.5%,#69a3ff 8780.84%);
                       border-radius: 90px;
                       text-transform: capitalize;
                     " class="py-7 px-4 mt-4 google-font"><strong>Register Now</strong>
-                                    </v-btn>
-                                    <v-btn dark large rounded outlined :href="`${communityData.link}`" target="_blank"
-                                        style="text-transform: capitalize;" class="mt-4 google-font"><strong>Learn
-                                            More</strong></v-btn>
-                                </div>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-row>
-            </v-carousel-item>
+                    </v-btn>
+                    <v-btn dark large rounded outlined :href="`${event.link}`" target="_blank"
+                      style="text-transform: capitalize;" class="mt-4 google-font"><strong>Learn
+                        More</strong></v-btn>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-carousel-item>
         </v-carousel>
-        <v-row align="center" justify="center" v-if="chapters.length == 0">
-            <v-col cols="12" sm="12" md="6" lg="4">
-                <div>
-                    <img src="../../assets/img/no-data-available.png" alt="no data" style="width:100%" />
-                </div>
-            </v-col>
-        </v-row>
+      </div>
+  
+      <div v-if="chunkedPastEvents.length > 0">
+        <h3 style="text-align: center; color: #EA4355;">Past Events</h3>
+        <v-carousel :show-arrows="true">
+          <v-carousel-item v-for="(chunk, index) in chunkedPastEvents" :key="index">
+            <v-row align="center" justify="center" class="py-10">
+              <v-col cols="12" sm="12" md="4" lg="4" v-for="(event, evIndex) in chunk" :key="evIndex">
+                <v-card class="pa-10"
+                  style="background: #000; border-color:#1f2023; border-width: 2px; border-radius: 60px; border-style: solid;">
+                  <div :class="`pa-5 mb-5 d-flex flex-column align-center ${getStyle(evIndex)}`"
+                    style="border-radius: 60px;">
+                    <h1><strong>{{ event.city }}</strong></h1>
+                  </div>
+                  <h2 class="text-white">On {{ getFormattedDate(event.date) }}</h2>
+                  <h5 class="text-white">at {{ event.place }}</h5>
+                  <div class="d-flex flex-column">
+                    <v-btn large rounded dark :href="`${event.rsvp}`" target="_blank" style="
+                      background: linear-gradient(145.24deg,#076eff 541.5%,#69a3ff 8780.84%);
+                      border-radius: 90px;
+                      text-transform: capitalize;
+                    " class="py-7 px-4 mt-4 google-font"><strong>Register Now</strong>
+                    </v-btn>
+                    <v-btn dark large rounded outlined :href="`${event.link}`" target="_blank"
+                      style="text-transform: capitalize;" class="mt-4 google-font"><strong>Learn
+                        More</strong></v-btn>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+      </div>
+  
+      <v-row align="center" justify="center" v-if="chunkedUpcomingEvents.length == 0 && chunkedPastEvents.length == 0">
+        <v-col cols="12" sm="12" md="6" lg="4">
+          <div>
+            <img src="../../assets/img/no-data-available.png" alt="no data" style="width:100%" />
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
-</template>
-
-<style>
-.blue-head {
+  </template>
+  
+  <style>
+  .blue-head {
     background-color: #076EFF33;
     color: #076EFF;
-}
-
-.red-head {
+  }
+  
+  .red-head {
     background-color: #EA435533;
     color: #EA4355;
-}
-
-.green-head {
+  }
+  
+  .green-head {
     background-color: #36A85233;
     color: #36A852;
-}
-
-.yellow-head {
+  }
+  
+  .yellow-head {
     background-color: #F4B40033;
     color: #F4B400;
-}
-</style>
+  }
+  </style>
+  
+  <script>
+  import moment from "moment";
+  import CommunityData from "@/assets/data/community.json";
 
-
-<script>
-
-import CommunityData from "@/assets/data/community.json";
-import moment from "moment";
-
-const CHUNK_SIZE = 3;
-const DATE_FORMAT = "DD/MM/YYYY";
-const DATE_DISPLAY_FORMAT = "D MMM YYYY"
-
-const chunkArray = (givenArray) => {
-    let finalArray = []
-    for (let i = 0; i < givenArray.length; i += CHUNK_SIZE) {
-        const chunk = givenArray.slice(i, i + CHUNK_SIZE);
-        finalArray.push(chunk)
+  
+  const DATE_FORMAT = "DD/MM/YYYY";
+  const DATE_DISPLAY_FORMAT = "D MMM YYYY";
+  let CHUNK_SIZE = 2;
+  if(window.innerWidth < 998) {
+    CHUNK_SIZE = 1;
+  }
+  
+  const chunkArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
     }
-    return finalArray;
-}
-export default {
+    return result;
+  };
+  
+  export default {
     methods: {
-        getStyle(indexNumber) {
-            switch (indexNumber % CHUNK_SIZE) {
-                default:
-                    return "blue-head";
-                case 1:
-                    return "green-head";
-                case 2:
-                    return "red-head"
-                case 3:
-                    return "yellow-head"
-            }
-        },
-        getformatedDate(givenDate) {
-            return moment(givenDate, DATE_FORMAT).format(DATE_DISPLAY_FORMAT)
-        }
+      getStyle(indexNumber) {
+        const styles = ["blue-head", "green-head", "red-head", "yellow-head"];
+        return styles[indexNumber % styles.length];
+      },
+      getFormattedDate(givenDate) {
+        return moment(givenDate, DATE_FORMAT).format(DATE_DISPLAY_FORMAT);
+      }
     },
     data: () => ({
-        chapters: chunkArray(CommunityData.filter(x => moment(x.date, DATE_FORMAT).isValid() && moment(x.date, DATE_FORMAT).isAfter(moment(), "date")).sort((a, b) => {
-            let dateA = moment(a.date, DATE_FORMAT);
-            let dateB = moment(b.date, DATE_FORMAT);
-            if (dateA.isSame(dateB, "date")) {
-                return 0;
-            }
-            return dateA.isBefore(dateB, "date") ? -1 : 1;
-        })),
-        certs: [
-            {
-                "type": "Associate",
-                "title": "Cloud Engineer",
-                "img": "ace.png"
-            },
-            {
-                "type": "Professional",
-                "title": "Data Engineer",
-                "img": "pde.png"
-            },
-            {
-                "type": "Professional",
-                "title": "Machine Learning Engineer",
-                "img": "pme.png"
-            },
-            {
-                "type": "Professional",
-                "title": "Cloud Architect",
-                "img": "pca.png"
-            },
-            {
-                "type": "Professional",
-                "title": "Cloud Developer",
-                "img": "pcd.png"
-            },
-            {
-                "type": "Professional",
-                "title": "Cloud DevOps Engineer",
-                "img": "pcdo.png"
-            }
-        ]
+      upcomingEvents: CommunityData.filter(event => moment(event.date, DATE_FORMAT).isAfter(moment(), "date")),
+      pastEvents: CommunityData.filter(event => moment(event.date, DATE_FORMAT).isBefore(moment(), "date")),
+      chunkedUpcomingEvents: chunkArray(
+        CommunityData.filter(event => moment(event.date, DATE_FORMAT).isAfter(moment(), "date")),
+        CHUNK_SIZE
+      ),
+      chunkedPastEvents: chunkArray(
+        CommunityData.filter(event => moment(event.date, DATE_FORMAT).isBefore(moment(), "date")),
+        CHUNK_SIZE
+      )
     })
-}
-</script>
+  };
+  </script>
